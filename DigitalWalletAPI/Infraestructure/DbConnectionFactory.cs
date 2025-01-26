b72bc3b2f738a5d47d8cb1f3acb335d6af78e469
MarlonnLima <goncalvesmarlon072@gmail.com>
2025-01-26 20:06:39 -0300

    Feat: configure connection to postgres

---
+++ DigitalWalletAPI/Infraestructure/DbConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using Npgsql;
+
+namespace DigitalWalletAPI.Infraestructure
+{
+    public class DbConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public DbConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new NpgsqlConnection(_connectionString);
+        }
+    }
+}
--- DigitalWalletAPI/Program.cs
+++ DigitalWalletAPI/Program.cs
@@ -1,5 +1,27 @@
+using DigitalWalletAPI.Domain.Repositories;
+using DigitalWalletAPI.Domain.Services;
+using DigitalWalletAPI.Infraestructure;
+using DotNetEnv;
+using Npgsql;
 var builder = WebApplication.CreateBuilder(args);
 
+// Setup postgres connectionString
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+Env.Load();
+
+var postgresIp = Env.GetString("POSTGRES_IP");
+var postgresPort = Env.GetString("POSTGRES_PORT");
+var postgresDb = Env.GetString("POSTGRES_DB");
+var postgresUser = Env.GetString("POSTGRES_USER");
+var postgresPassword = Env.GetString("POSTGRES_PASSWORD");
+
+connectionString = connectionString
+        .Replace("POSTGRES_IP", postgresIp)
+        .Replace("POSTGRES_PORT", postgresPort)
+        .Replace("POSTGRES_DB", postgresDb)
+        .Replace("POSTGRES_USER", postgresUser)
+        .Replace("POSTGRES_PASSWORD", postgresPassword);
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -7,14 +29,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// DI
+builder.Services.AddSingleton(sp => new DbConnectionFactory(connectionString));
+builder.Services.AddTransient<UserRepository>();
+builder.Services.AddTransient<UserService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
