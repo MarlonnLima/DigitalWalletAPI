0e2a01ca6ffd66f652ef98c651ea600f9422824a
MarlonnLima <goncalvesmarlon072@gmail.com>
2025-01-26 20:07:49 -0300

    Feat: add UserService

---
+++ DigitalWalletAPI/Domain/Services/UserService.cs
@@ -0,0 +1,35 @@
+﻿using DigitalWalletAPI.Domain.Entities;
+using DigitalWalletAPI.Domain.Repositories;
+using Npgsql;
+
+namespace DigitalWalletAPI.Domain.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _userRepository;
+
+        public UserService(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public User GetById(int Id)
+        {
+            if (Id <= 0)
+            {
+                throw new ArgumentException("O id é inválido");
+            }
+
+            User user = _userRepository.FindById(Id);
+
+            if (user == null)
+            {
+                throw new NpgsqlException("O usuário não foi encontrado");
+            }
+            else
+            {
+                return user;
+            }
+        }
+    }
+}
