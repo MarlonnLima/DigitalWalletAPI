2ba00177c4203c99ad8f8c4d95c4a9d74b16eaca
MarlonnLima <goncalvesmarlon072@gmail.com>
2025-01-26 20:07:36 -0300

    Feat: Add UserRepository

---
+++ DigitalWalletAPI/Domain/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using DigitalWalletAPI.Domain.Entities;
+using DigitalWalletAPI.Infraestructure;
+
+namespace DigitalWalletAPI.Domain.Repositories
+{
+    public class UserRepository
+    {
+        private readonly DbConnectionFactory _connectionFactory;
+
+        public UserRepository(DbConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public User FindById(int id)
+        {
+            try
+            {
+                using (var conn = _connectionFactory.CreateConnection())
+                {
+                    conn.Open();
+                    var user = conn.QueryFirstOrDefault<User>("SELECT * FROM USERS WHERE ID = @ID", new { ID = id });
+                    return user;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return null;
+        }
+    }
+}
